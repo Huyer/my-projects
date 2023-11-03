14630f11acdc9706f09a1ed94e1e92e489f00503
huy <tranvanhuy0122@gmail.com>
2023-11-03 11:28:23 +0700

    update CustomButton

--- car_showcase/src/components/CustomButton.tsx
+++ car_showcase/src/components/CustomButton.tsx
@@ -9,14 +9,29 @@ const CustomButton = ({
   containerStyles,
   handleClick,
   btnType,
+  textStyle,
+  rightIcon,
+  isDisabled,
 }: CustomButtonProps) => {
   return (
     <button
       type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
+      disabled={false}
     >
-      {title}
+      <span className={`flex-1 ${textStyle}`}>{title}</span>
+
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="rightIcon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
